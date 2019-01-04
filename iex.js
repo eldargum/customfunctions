071eb2b3a4b8c74bff9d81fdd151dcbcf0eefde4
eldargum <eldar_gumurzakov@hotmail.com>
2019-01-04 11:58:16 +0500

    Add files via upload

---
+++ iex.js
@@ -0,0 +1,34 @@
+function stockPriceStream(ticker, handler) {
+    var updateFrequency = 1000 /* milliseconds*/;
+    var isPending = false;
+
+    var timer = setInterval(function() {
+        // If there is already a pending request, skip this iteration:
+        if (isPending) {
+            return;
+        }
+
+        var url = "https://api.iextrading.com/1.0/stock/" + ticker + "/price";
+        isPending = true;
+
+        fetch(url)
+            .then(function(response) {
+                return response.text();
+            })
+            .then(function(text) {
+                handler.setResult(parseFloat(text));
+            })
+            .catch(function(error) {
+                handler.setResult(error);
+            })
+            .then(function() {
+                isPending = false;
+            });
+    }, updateFrequency);
+
+    handler.onCanceled = () => {
+        clearInterval(timer);
+    };
+}
+
+CustomFunctionMappings.STOCKPRICESTREAM = stockPriceStream;
